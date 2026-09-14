d8a79d0d097322946e594a16f44c03c3541af83e
michelle <michelle.graham@dkit.ie>
2026-09-15 00:34:40 +0100

    Initial commit

---
+++ src/main/java/WebPatterns_InClass/WebPatternsInClass202627Application.java
@@ -0,0 +1,13 @@
+package WebPatterns_InClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPatternsInClass202627Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPatternsInClass202627Application.class, args);
+    }
+
+}
---
+++ src/test/java/WebPatterns_InClass/WebPatternsInClass202627ApplicationTests.java
@@ -0,0 +1,13 @@
+package WebPatterns_InClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPatternsInClass202627ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
